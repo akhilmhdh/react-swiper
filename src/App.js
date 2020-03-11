691e53d56671c3aac331c927771f8722ae1af73b
akhilmhdh <akhilmhdh@gmail.com>
2020-03-11 22:33:23 +0530

    =setting eveything up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-
+import React,{Component} from 'react';
+import 'swiper/css/swiper.min.css'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state=({index:0,category:['All','Lorem','Ipsum','Dolor']})
+  }
+  render(){
+    return (
+      <div className="App">
+        <div className="filterContainer">
+          {/* This will be a filter to apply a filter function to swiper slides. Very basic filter :)*/}
+          <div className="filterGoLeft"
+          onClick={()=>{
+            this.setState({index:--this.state.index<0?this.state.category.length-1:this.state.index})
+          }}>&lt;</div>
+          <div className="filterData">{this.state.category[this.state.index]}</div>
+          <div className="filterGoLeft"
+          onClick={()=>{
+            this.setState({index:(this.state.index+1)%this.state.category.length})
+          }}
+          >&gt;</div>
+        </div>
+        <div className="swiperMainContainer">
+          {/* Here we will be adding swiper container */}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
